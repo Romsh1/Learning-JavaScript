9cdf0a73554674adc75763e5f87c9653443b968a
Romika <romika2002chau.21@gmail.com>
2024-02-07 16:58:01 -0500

    first commit

---
+++ first.js
@@ -0,0 +1,92 @@
+// console.log("My first JavaScript Project.");
+// console.log("Romika Chaudhary");
+// console.log("I love JavaScript.");
+
+// fullName = "Tony Stark";
+// x = null;
+// y = undefined;
+// isFollow = true;
+// console.log(isFollow);
+
+
+//Create an array of random numbers and using fat arrow function find max, min, avg, mode and median
+// let randomNumbers = Array.from({ length: 10 }, () => Math.floor(Math.random() * 100) + 1);
+// console.log(randomNumbers);
+
+// let maxNumber = Math.max(...randomNumbers);
+// console.log("Maximum random number:", maxNumber);
+
+// let minNumber = Math.min(...randomNumbers);
+// console.log("Minimum random numbers:", minNumber);
+
+// function calculateAverageOfArray(randomNumbers) {
+//     var total = 0;
+//     var count = 0;
+
+//     jQuery.each(randomNumbers, function(index, value) {
+//         total += value;
+//         count++;
+//     });
+
+//     return total / count;
+// }
+
+// console.log(calculateAverageOfArray(arry));
+
+// let avgNumber = Math.avg(...randomNumbers);
+// console.log("Average number:", calculateAverageOfArray(randomNumbers));
+
+// let fullName = "Romika Chaudhary";
+// fullName = "Amanu";
+// console.log(fullName);
+
+// const PI = 3.14;
+// console.log(PI);
+
+// let age = 22;
+// console.log(typeof(age));
+
+// isFollow = true;
+// typeof is
+
+// console.log("Hey");
+
+
+// let x = BigInt("123");
+// let y = Symbol("Hello");
+
+// const student = {
+//     fullName : "Romika Chaudhary",
+//     age : 22,
+//     gpa : 3.77,
+//     isPass : true
+// };
+
+
+// const product = {
+//     prodName : "Parker Jotter Standard CT Ball Pen (Black)",
+//     rating : 4,
+//     price : 270,
+//     offer: 5
+// };
+
+const profile = {
+    username: "shradhakhapra",
+    noOfPosts: 195,
+    followers: "569K",
+    following: 4,
+    occupation: "Entrepreneur",
+    quote: "To educate someone is the highest privilege"
+};
+
+console.log(typeof profile["username"]);
+
+
+//Unary Operator
+let a = 5;
+let b = 2;
+
+console.log("a = ", a, " & b = ", b);
+a += 1;
+console.log("a = ",a);
+
