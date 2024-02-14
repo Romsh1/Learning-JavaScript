bc28aa926a2c5ed1baeaf5f2e264783264c7fa4e
Romika <romika2002chau.21@gmail.com>
2024-02-13 22:30:28 -0500

    Day 10 - Fetch API using promise chaining

--- Day10/fetchApi.js
+++ Day10/fetchApi.js
@@ -5,33 +5,33 @@ Day 10
 Feb 13, 2024
 */
 
-// const URL = "https://cat-fact.herokuapp.com/facts";
-// const factPara = document.querySelector("#fact");
-// const factBtn = document.querySelector("#btn");
+const URL = "https://cat-fact.herokuapp.com/facts";
+const factPara = document.querySelector("#fact");
+const factBtn = document.querySelector("#btn");
 
 //Using async-await
-// const getFacts = async () => {
-//     console.log('Getting data...');
-//     let response = await fetch(URL);
-//     console.log(response);  //JSON
-//     let data = await response.json();
-//     factPara.innerText = data[0].text;
-// };
+const getFacts = async () => {
+    console.log('Getting data...');
+    let response = await fetch(URL);
+    console.log(response);  //JSON
+    let data = await response.json();
+    factPara.innerText = data[0].text;
+};
 
 
 /*Fetch API using 
 Promise chaining
 */
-// function getFacts() {
-//     fetch(URL).then((response) => {
-//         return response.json();
-//     })
-//     .then((data) => {
-//         console.log(data);
-//     });
-// }
+function getFacts() {
+    fetch(URL).then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data);
+    });
+}
 
-// factBtn.addEventListener("click", getFacts);
+factBtn.addEventListener("click", getFacts);
 
 
 
