06dd25c0f8b7d43b79e1672cae1d4c13fb526582
Romika <romika2002chau.21@gmail.com>
2024-03-13 16:49:26 -0400

    Simple programs to understand about arrays and objects - Day 35

---
+++ Day35/script.js
@@ -0,0 +1,17 @@
+// Romika Chaudhary
+// Day 35
+
+
+//Program to greet a person with their firstname and lastname
+const firstName = "Romika";
+const lastName = "Chaudhary";
+console.log('Hello '+ firstName + ' ' + lastName + '.');
+
+//counting sum from 0 to 100
+let i = 0;
+let sum = 0;
+for (i; i <= 100; i++) {
+    sum += i;
+}
+console.log('The sum of 0 to 100 is ' + sum);
+
