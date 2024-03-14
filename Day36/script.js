874ec189f66ba39902c3f498629222f3b2f264c1
Romika <romika2002chau.21@gmail.com>
2024-03-14 15:14:46 -0400

    Learned about callbacks - Day 36

---
+++ Day36/script.js
@@ -0,0 +1,31 @@
+// Romika Chaudhary
+// Day 36
+// Loops, Functions and Callbacks
+
+//function to find the square
+function square(n) {
+    return n * n;
+}
+
+//function to find the cube
+function cube(n) {
+    return n * n* n;
+}
+
+//function to calculate sum of squares
+function sumOfSquare(a, b) {
+    const val1 = square(a);
+    const val2 = square(b);
+    return val1 + val2;
+}
+
+//function to calculate sum of cubes
+function sumOfCube(a,b,c) {
+    const val1 = cube(a);
+    const val2 = cube(b);
+    const val3 = cube(3);
+    return val1 + val2 + val3;
+}
+
+console.log("The sum of cube is " + sumOfCube(4, 9, 5) + ".");
+console.log("The sum of square is " + sumOfSquare(4, 9) + ".");
