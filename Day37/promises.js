a0e0eb23c65081fdca1857caaf3dff893877e34d
Romika <romika2002chau.21@gmail.com>
2024-03-15 23:37:49 -0400

    Learned about promises - Day 37

--- Day37/promises.js
+++ Day37/promises.js
@@ -3,3 +3,33 @@
 // Day 37
 // Promises
 
+function kiratsReadFile() {
+    console.log("inside kiratsreadfile");
+    return new Promise(function(resolve) {
+        console.log("inside promise");
+        fs.readFile("sentence.txt", "utf-8", function(err, data){
+            console.log("before resolve");
+            resolve(data);
+        });
+    })
+}
+
+function onDone(data) {
+    console.log(data)
+}
+
+kiratsReadFile().then(onDone);
+
+
+var d = new Promise(function(resolve) {
+    setTimeout(function() {
+        resolve("foo");
+
+    }, 1000)
+});
+
+function callback() {
+    console.log(d);
+}
+
+d.then(callback)
