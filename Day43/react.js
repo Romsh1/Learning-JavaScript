2245d490f37e56281a3d27e244afbc8f90ba1a44
Romika <romika2002chau.21@gmail.com>
2024-03-26 00:46:13 -0400

    Basics of React - Day 43

--- Day43/react.js
+++ Day43/react.js
@@ -4,3 +4,29 @@
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<h1>Hello, World</h1>);
+
+
+const name = 'Josh Perez';
+// const element = <h1>Hello, World!</h1>;
+
+function formatName(user) {
+    return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+    firstName: 'Harper',
+    lastName: 'Perez'
+};
+
+const element = (
+    <h1>
+        Hello, {formatName(user)}!
+    </h1>
+);
+
+function getGreeting(user) {
+    if (user) {
+        return <h1>Hello, {formatName(user)}!</h1>;
+    }
+    return <h1>Hello, Starnger.</h1>;
+}
