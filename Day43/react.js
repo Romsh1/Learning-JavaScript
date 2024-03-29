27d07c6440ed2a40f642d29db71a4f6b1bdab7fd
Romika <romika2002chau.21@gmail.com>
2024-03-28 23:45:09 -0400

    Day 42 of Learning JS

--- Day43/react.js
+++ Day43/react.js
@@ -28,5 +28,5 @@ function getGreeting(user) {
     if (user) {
         return <h1>Hello, {formatName(user)}!</h1>;
     }
-    return <h1>Hello, Starnger.</h1>;
+    return <h2>Hello, Starnger.</h2>;
 }
