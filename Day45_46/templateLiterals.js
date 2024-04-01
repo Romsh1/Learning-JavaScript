b931ff6989255bf839c86267b1b2e50df7bb10ed
Romika <romika2002chau.21@gmail.com>
2024-04-01 00:06:21 -0400

    Optimal chaining in JS

--- Day45_46/templateLiterals.js
+++ Day45_46/templateLiterals.js
@@ -150,7 +150,7 @@ const data = [
   }
   
 //Destructuring
-const book = getBook(2);
+const book = getBook(4);
 book;
 // const title = book.title;
 // const genre = book.genres;
@@ -198,12 +198,28 @@ const pagesRange = pages > 1000 ? 'Over a thousand' : 'less than 1000';
 pagesRange;
 
 //Short-circuiting and Logical Operator
+console.log(true && 'Some string');
+console.log(false && 'Some string');
+console.log(hasMovieAdaptation && 'This book has a movie');
+
+//falsy:0, '', null
+console.log('jonas' && 'Some string');
+console.log(0 && 'Some string');
+
+console.log(true || 'Some string');
+console.log(false || 'Some string');
+
+console.log(book.translations.spanish);
+
+const spanishTranslation = book.translations.spanish || "NOT TRANSLATED";
+spanishTranslation;
+
 console.log(book.reviews.librarything.reviewsCount);
 const countWrong = book.reviews.librarything.ratingsCount || "no data";
 countWrong;
 
 const count = book.reviews.librarything.reviewsCount ?? "no data";
-
+count;
 
 //Optimal Chaining
 function getTotalReviewCount(book) {
