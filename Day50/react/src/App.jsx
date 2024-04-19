9c102687b561cc324e39963aafeb1bc89b81ffbd
Romika <romika2002chau.21@gmail.com>
2024-04-19 00:41:26 -0400

    Implemented prop and PropTypes

--- Day50/react/src/App.jsx
+++ Day50/react/src/App.jsx
@@ -5,7 +5,10 @@ function App() {
 
   return (
     <>
-      <Student name="Spongebob1" age={15} isStudent={true}/>
+      <Student name="Spongebob1" age={30} isStudent={true}/>
+      <Student name="Romika" age={22} isStudent = {true}/>
+      <Student name="Rohan" age={26} isStudent = {false}/>
+      <Student name="Lora"/>
     </>
   );
 }
