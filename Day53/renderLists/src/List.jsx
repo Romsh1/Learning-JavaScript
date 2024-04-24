75179f75c79a2a4c8578b5eb6a7d6531b5823f82
Romika <romika2002chau.21@gmail.com>
2024-04-24 17:30:47 -0400

    Implemeted sorting an array.

--- Day53/renderLists/src/List.jsx
+++ Day53/renderLists/src/List.jsx
@@ -1,5 +1,7 @@
 function List() {
     const fruits = ["apple", "orange", "banana", "coconut", "pineapple"];
+    fruits.sort();
+    
     const listItems = fruits.map(fruit => <li>{fruit}</li>);
     
     return(<ol>{listItems}</ol>);
