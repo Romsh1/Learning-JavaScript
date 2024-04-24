3144a784247d13545633a57969bec4b44999c731
Romika <romika2002chau.21@gmail.com>
2024-04-24 17:00:34 -0400

    Printing list as ordered and unordered list

---
+++ Day53/renderLists/src/List.jsx
@@ -0,0 +1,7 @@
+function List() {
+    const fruits = ["apple", "orange", "banana", "coconut", "pineapple"];
+    const listItems = fruits.map(fruit => <li>{fruit}</li>);
+    
+    return(<ol>{listItems}</ol>);
+}
+export default List
