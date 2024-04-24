ba55c17b58e50f27a0c37d2b02e99371d4104d35
Romika <romika2002chau.21@gmail.com>
2024-04-24 18:32:50 -0400

    Used localeCompare method

--- Day53/renderLists/src/List.jsx
+++ Day53/renderLists/src/List.jsx
@@ -1,8 +1,15 @@
 function List() {
-    const fruits = ["apple", "orange", "banana", "coconut", "pineapple"];
-    fruits.sort();
+    const fruits = [{name: "apple", calories: 95}, 
+                    {name: "orange", calories: 45},
+                    {name: "banana",  calories: 105},
+                    {name: "coconut",  calories: 159}, 
+                    {name: "pineapple",  calories: 37}];
+    //fruits.sort((a, b) => a.name.localeCompare(b.name)); alphabetical
+    fruits.sort((a, b) => b.name.localeCompare(a.name)); //reverse alphabetical
     
-    const listItems = fruits.map(fruit => <li>{fruit}</li>);
+    const listItems = fruits.map(fruit => <li key={fruit.name}>
+                                            {fruit.name}: &nbsp;
+                                            <b>{fruit.calories}</b></li>);
     
     return(<ol>{listItems}</ol>);
 }
