10187338ba5eb4f04595caafcf08fa619ca236e1
Romika <romika2002chau.21@gmail.com>
2024-04-25 00:55:34 -0400

    Implemented event property - Day 54 of learning JS and React

--- Day54/react/src/Button.jsx
+++ Day54/react/src/Button.jsx
@@ -1,16 +1,18 @@
 function Button() {
-    let count = 0;
-    const handleClick = (name) => {
-        if(count < 3) {
-            count++;
-            console.log(`${name} clicke me ${count} time/s`);
+    // let count = 0;
+    // const handleClick = (name) => {
+    //     if(count < 3) {
+    //         count++;
+    //         console.log(`${name} clicke me ${count} time/s`);
 
-        }else {
-            console.log(`${name} stop clicking me`);
-        }
-    };
-    // const handleClick2 = (name) => console.log(`${name} stop clicking me`);
+    //     }else {
+    //         console.log(`${name} stop clicking me`);
+    //     }
+    // };
+    // // const handleClick2 = (name) => console.log(`${name} stop clicking me`);
+//    return(<button onClick={() => handleClick("Bob")}>Click me</button>);
 
-    return(<button onClick={() => handleClick("Bob")}>Click me</button>);
+    const handleClick = (e) => e.target.textContent = "Ouch";
+    return(<button onDoubleClick={(e) => handleClick(e)}>Click me</button>);
 }
 export default Button
