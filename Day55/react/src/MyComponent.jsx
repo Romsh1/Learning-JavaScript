fd512b65eed7180eb94a174b20ef3b10c8b47fa8
Romika <romika2002chau.21@gmail.com>
2024-04-26 17:19:18 -0400

    Added input field for quantity

--- Day55/react/src/MyComponent.jsx
+++ Day55/react/src/MyComponent.jsx
@@ -2,15 +2,22 @@ import React, {useState} from "react";
 
 function MyComponent() {
     const [name, setName] = useState("Guest");
+    const [quantity, setQuantity] = useState(0);
 
     function handleNameChange(event) {
         setName(event.target.value);
+    }
+
+    function handleQuantityChange(event) {
+        setQuantity(event.target.value);
     }
 
     return(<div>
                 <h1>Hello</h1>
                 <input value={name} onChange={handleNameChange}/>
                 <p>Name: {name}</p>
+                <input value={quantity} onChange={handleQuantityChange} type="number" />
+                <p>Quantity: {quantity}</p>
             </div>)
 }
 export default MyComponent
