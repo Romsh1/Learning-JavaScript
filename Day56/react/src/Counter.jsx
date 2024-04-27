5f9756512fc48647e2b8fbcc9dad778ce79e4eda
Romika <romika2002chau.21@gmail.com>
2024-04-27 01:13:30 -0400

    Updated OBJECTS in state

---
+++ Day56/react/src/Counter.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+function Counter() {
+    const [count, setCount] = useState(0);
+
+    function increment() {
+        setCount(c => c + 1);
+    };
+
+    function decrement() {
+        setCount(c => c - 1);
+    };
+
+    function reset() {
+        setCount(c => c = 0);
+    };
+
+    return(
+        <div>
+            <p>Count: {count}</p>
+            <button onclick={decrement}>Decrement</button>
+            <button onclick={increment}>Increment</button>
+            <button onclick={reset}>Reset</button>
+        </div>
+    );
+}
+export default Counter
--- Day56/react/src/MyComponent.jsx
+++ Day56/react/src/MyComponent.jsx
@@ -1,27 +1,31 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 
 function MyComponent() {
-    const [count, setCount] = useState(0);
 
-    function increment() {
-        setCount(c => c + 1);
-    };
+    const [car, setCar] = useState({year: 2024, 
+                                    make: "ford", 
+                                    model: "Mustang"});
 
-    function decrement() {
-        setCount(c => c - 1);
-    };
+    function handleYearChange(event) {
+        setCar(c => ({...c, year: event.target.value}));
+    }
 
-    function reset() {
-        setCount(c => c = 0);
-    };
+    function handleMakeChange(event) {
+        setCar(c => ({...c, make: event.target.value}));
+    }
 
-    return(
-        <div>
-            <p>Count: {count}</p>
-            <button onclick={decrement}>Decrement</button>
-            <button onclick={increment}>Increment</button>
-            <button onclick={reset}>Reset</button>
-        </div>
-    );
+    function handleModelChange(event) {
+        setCar(c => ({...c, model: event.target.value}));
+    }
+
+
+    return(<div>
+            <p>Your favorite car is: {car.year} {car.make} {car.model}</p>
+            <input type="number" value={car.year} onChange={handleYearChange}/><br/>
+            <input type="text" value={car.make} onChange={handleMakeChange}/><br/>
+            <input type="text" value={car.model} onChange={handleModelChange}/><br/>
+        </div>);
 }
 export default MyComponent
+
+
