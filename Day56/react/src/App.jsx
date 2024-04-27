a7569a1f543db66d0bd04d2e57e28f47b88f82f1
Romika <romika2002chau.21@gmail.com>
2024-04-27 01:26:46 -0400

    Implemented update arrays in state - Day 56 of Learning JS and React

--- Day56/react/src/App.jsx
+++ Day56/react/src/App.jsx
@@ -1,7 +1,7 @@
-import MyComponent from "./MyComponent.jsx"
+import MyComponent2 from "./MyComponent2.jsx"
 
 function App() {
-  return (<MyComponent />);
+  return (<MyComponent2 />);
 }
 
 export default App
