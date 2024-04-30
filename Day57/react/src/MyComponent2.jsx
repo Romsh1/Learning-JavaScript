4e50195917ec36c5671571b2fc810ebc5929802e
Romika <romika2002chau.21@gmail.com>
2024-04-29 22:31:45 -0400

    Learing about useEffect() hook

--- Day57/react/src/MyComponent2.jsx
+++ Day57/react/src/MyComponent2.jsx
@@ -2,18 +2,29 @@ import React, {useState, useEffect} from "react";
 
 function MyComponent2() {
     const [count, setCount] = useState(0);
+    const [color, setColor] = useState("Green");
 
     useEffect(() => {
-        document.title = `Count: ${count}`;
-    });
+        document.title = `Count: ${count} ${color}`;
+    }, [count, color]);
 
     function addCount() {
         setCount(c => c + 1);
     }
 
+    function subtractCount() {
+        setCount(c => c -1);
+    }
+
+    function changeColor() {
+        setColor(c => c === "Green" ? "red" : "Green");
+    }
+
     return(<>
-        <p>Count: {count}</p>
+        <p style={{color: color}}>Count: {count}</p>
         <button onClick={addCount}>Add</button>
+        <button onClick={subtractCount}>Subtract</button><br/>
+        <button onClick={changeColor}>Change Color</button>
     </>);
 }
 export default MyComponent2
