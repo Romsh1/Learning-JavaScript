5b5b36c56d885c1ae26de677f57cbe68388bf726
Romika <romika2002chau.21@gmail.com>
2024-05-03 18:46:23 -0400

    Created a BMI calculator using the React and the concept of using hooks in React - Day 60 of lerning React and JS

--- BMI Calculator - Day60/src/BmiCalc.jsx
+++ BMI Calculator - Day60/src/BmiCalc.jsx
@@ -6,25 +6,61 @@ function BmiCalc() {
     const [bmiValue, setBmiValue] = useState("");
     const [bmiMessage, setBmiMessage] = useState("");
 
-    function handleHeight() {
-        setHeight(heigth.target.value);
-    }
+    // function handleHeight() {
+    //     setHeight(heigth.target.value);
+    // }
 
-    function handleWeight() {
-        setHeight(weigth.target.value);
-    }
+    // function handleWeight() {
+    //     setWeight(weigth.target.value);
+    // }
 
+    const calculateBmi = () => {
+        if (heigth && weight) {
+            const heightInMeters = heigth / 100;
+            const bmi = (weight / (heightInMeters * heightInMeters)).toFixed(2);
+            setBmiValue(bmi);
+
+            let message = "";
+            if (bmi < 18.5) {
+                message = "You are underweight";
+            } else if (bmi >= 18.5 && bmi < 25) {
+                message = "You are normal weight";
+            } else if (bmi >= 25 && bmi < 30) {
+                message = "You are overweight";
+            } else {
+                message = "You are obese";
+            }
+            setBmiMessage(message);
+        } else {
+            setBmiValue("");
+            setBmiMessage("");
+        }
+    };
 
     return(<div className="container"> 
                 <h1>Romika's BMI Calculator</h1>
                 <div className="input-container">
-                    <label type="text" id="height">Enter your height</label>
-                    <input type="text" id="typeHeight" value={heigth} onChange={handleHeight} />
+                    <label id="height">Enter your height</label>
+                    <input type="number" id="typeHeight" value={heigth} 
+                    onChange={(e) => setHeight(e.target.value)} />
                 </div>
                 <div className="input-container">
                     <label id="weight">Enter your weight</label>
-                    <input type="text" id="typeWeight" value={weight} onChange={handleWeight} />
+                    <input type="number" id="typeWeight" value={weight} 
+                    onChange={(e) => setWeight(e.target.value)} />
                 </div>
+                <button className="calculate-btn" onClick={calculateBmi}>Calculate BMI</button>
+
+                {bmiValue && bmiMessage  && (
+                    <div className="result">
+                        <p>
+                            Your BMI: <span className="bmi-value">{bmiValue}</span>
+                        </p>
+                        <p>
+                            Result: <span className="bmi-message">{bmiMessage}</span>
+                        </p>
+                    </div>
+                )}
             </div>
     );
 }
