3eeb4455119ce4fa0e821cd85ed41e111bc87582
Romika <romika2002chau.21@gmail.com>
2024-05-13 23:11:00 -0400

    Implemented some event listener functions in React - Day 65 of Learning React

--- steps/src/App.js
+++ steps/src/App.js
@@ -7,6 +7,14 @@ const messages = [
 function App() {
   const step = 3;
 
+  function handlePrevious() {
+    alert("Previous");
+  }
+
+  function handleNext() {
+    alert("Next");
+  }
+
   return <div className="steps">
     <div className="numbers">
       <div className={`${step >= 1 ? 'active' : ""}`}>1</div>
@@ -17,10 +25,16 @@ function App() {
     <p className="message">Step {step}: {messages[step - 1]}</p>
 
     <div className="buttons">
-      <button style={{backgroundColor: "7950f2", color: "white"}}>
+      <button 
+        style={{backgroundColor: "7950f2", color: "white"}} 
+        onClick={handlePrevious}
+      >
         Previous
       </button>
-      <button style={{backgroundColor: "7950f2", color: "white"}}>
+      <button 
+        style={{backgroundColor: "7950f2", color: "white"}}
+        onClick={handleNext}
+      >
         Next
       </button>
     </div>
