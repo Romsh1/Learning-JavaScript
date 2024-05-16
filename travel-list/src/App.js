3a6061a889df7b9b44be61aed8c285939686a03b
Romika <romika2002chau.21@gmail.com>
2024-05-16 17:41:27 -0400

    Created state variable for all the components

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,4 +1,27 @@
 function App() {
 
 }
+
+function Logo() {
+  return <h1>🌴 Far Away 👜</h1>
+}
+
+function Form() {
+  return <div className="add-form">
+    <h3>What do you need for your trip? 😁</h3>
+  </div>
+}
+
+function PackingList() {
+  return <div className="list">LIST</div>
+}
+
+function Stats() {
+  return <footer>
+    <em>
+    🎒You have x items on your list, and you already packed x (x%)
+    </em>
+  </footer>
+}
+
 export default App
