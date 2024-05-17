8ca629c8b7323253a0c0dbb05d77edcd847a3941
Romika <romika2002chau.21@gmail.com>
2024-05-16 21:23:09 -0400

    Update

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,3 +1,8 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
 function App() {
   return(
     <div className="app">
@@ -20,11 +25,23 @@ function Form() {
 }
 
 function PackingList() {
-  return <div className="list">LIST</div>
+  return(
+    <div className="list">
+      <ul>
+        {initialItems.map((item) =>(
+          <Item item={item} />
+        ))}
+      </ul>
+    </div> 
+  );
+}
+
+function Item({item}) {
+  return <li>{item.description}</li>
 }
 
 function Stats() {
-  return <footer>
+  return <footer className="stats">
     <em>
     🎒You have x items on your list, and you already packed x (x%)
     </em>
