3998c26807024ba67b55ea3a189c567d938f155a
Romika <romika2002chau.21@gmail.com>
2024-05-17 20:37:58 -0400

    Update on recent component

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -24,7 +24,11 @@ function Form() {
       <h3>What do you need for your trip? 😁</h3>
       <select>
         {Array.from({length: 20}, (_, i) => i + 1).
-        map(num=><option value={num} key={num}>{num}</option>)}
+        map(num=>
+        <option value={num} key={num}>
+          {num}
+        </option>
+      )}
       </select>
       <input type='text' placeholder="Item..." />
       <button>Add</button>
