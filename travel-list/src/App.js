401bde3dad06519c27153dfed7e0389b8808c724
Romika <romika2002chau.21@gmail.com>
2024-05-17 20:37:17 -0400

    Added a form element

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -19,9 +19,17 @@ function Logo() {
 }
 
 function Form() {
-  return <div className="add-form">
-    <h3>What do you need for your trip? 😁</h3>
-  </div>
+  return (
+    <form className="add-form">
+      <h3>What do you need for your trip? 😁</h3>
+      <select>
+        {Array.from({length: 20}, (_, i) => i + 1).
+        map(num=><option value={num} key={num}>{num}</option>)}
+      </select>
+      <input type='text' placeholder="Item..." />
+      <button>Add</button>
+    </form>
+  );
 }
 
 function PackingList() {
