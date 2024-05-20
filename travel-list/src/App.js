6e5b017826c283616e7e415ddb2ec857421de9fa
Romika <romika2002chau.21@gmail.com>
2024-05-20 00:53:10 -0400

    Lifting state up

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -6,11 +6,17 @@ const initialItems = [
 ];
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
   return(
     <div className="app">
         <Logo />
-        <Form />
-        <PackingList />
+        <Form onAddItems={handleAddItems} />
+        <PackingList items={items} />
         <Stats />
     </div>  
   );
@@ -20,7 +26,7 @@ function Logo() {
   return <h1>🌴 Far Away 👜</h1>
 }
 
-function Form() {
+function Form({onAddItems}) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
 
@@ -33,6 +39,10 @@ function Form() {
       id: Date.now() 
     };
     console.log(newItem);
+
+    onAddItems(newItem);
+
+    handleAddItems(newItem);
 
     setDescription('');
     setQuantity(1);
@@ -57,11 +67,11 @@ function Form() {
   );
 }
 
-function PackingList() {
+function PackingList({items}) {
   return(
     <div className="list">
       <ul>
-        {initialItems.map((item) =>(
+        {items.map((item) =>(
           <Item item={item} key={item.id} />
         ))}
       </ul>
