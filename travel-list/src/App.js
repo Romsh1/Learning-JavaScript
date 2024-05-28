102c3769888be0d5536889bd653804dc48d78add
Romika <romika2002chau.21@gmail.com>
2024-05-28 03:44:55 -0700

    Added a sort button

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -86,10 +86,24 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
     <div className="list">
       <ul>
         {items.map((item) =>(
-          <Item item={item} onDeleteItem={onDeleteItem} 
-          onToggleItem = {onToggleItem} key={item.id} />
+          <Item item={item} 
+            onDeleteItem={onDeleteItem} 
+            onToggleItem = {onToggleItem} 
+            key={item.id} 
+          />
         ))}
       </ul>
+
+      <div className="actions">
+        <select>
+          <option value='input'>Sort by input order
+          </option>
+          <option value='description'>Sort by description
+          </option>
+          <option value='packed'>Sort by packed status
+          </option>
+        </select>
+      </div>
     </div> 
   );
 }
