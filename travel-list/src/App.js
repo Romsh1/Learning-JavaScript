0c665fb224b2fc268ed79e014a0fe6b184b7f815
Romika <romika2002chau.21@gmail.com>
2024-05-28 03:40:15 -0700

    Update on display message

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -108,6 +108,15 @@ function Item({ item, onDeleteItem, onToggleItem }) {
 }
 
 function Stats( {items} ) {
+  if (!items.length) {
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list
+           📃</em>
+      </p>
+    );
+  }
+
   const numItems = items.length;
   const numPacked = items.filter(item=>item.packed).length;
   const percentage = Math.round( (numPacked / numItems) * 100);
