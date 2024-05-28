6bce9f60dd930643cd9f48c86b59a98871ba0fa7
Romika <romika2002chau.21@gmail.com>
2024-05-28 04:12:38 -0700

    Added a function clear up list

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -26,13 +26,23 @@ function App() {
   );
   }
 
+  function handleClearList() {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete all items?"
+    );
+
+    if (confirmed) setItems([]);
+  }
 
   return(
     <div className="app">
         <Logo />
         <Form onAddItems={handleAddItems} />
         <PackingList items={items}
-          onDeleteItem = {handleDeleteItem} onToggleItem = {handleToggleItem} />
+          onDeleteItem = {handleDeleteItem} 
+          onToggleItem = {handleToggleItem}
+          onClearList = {handleClearList}
+          />
         <Stats items = {items} />
     </div>  
   );
@@ -81,7 +91,7 @@ function Form({onAddItems}) {
   );
 }
 
-function PackingList({ items, onDeleteItem, onToggleItem }) {
+function PackingList({ items, onDeleteItem, onToggleItem, onClearList }) {
   const [sortBy, setSortBy] = useState("input");
 
   let sortedItems;
@@ -117,6 +127,7 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
           <option value='packed'>Sort by packed status
           </option>
         </select>
+        <button onClick={onClearList}>Clear List</button>
       </div>
     </div> 
   );
