45cca5ecff67d6eb6e7778dcf6b03d2ac73dbcb8
Romika <romika2002chau.21@gmail.com>
2024-05-29 23:28:05 -0700

    Moved components to separate files - Day 73 of Learning React and JS

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,4 +1,6 @@
 import {useState} from "react";
+import Logo from "./Logo.js";
+import Form from "./Form.js"
 
 // const initialItems = [
 //   { id: 1, description: "Passports", quantity: 2, packed: false },
@@ -45,49 +47,6 @@ function App() {
           />
         <Stats items = {items} />
     </div>  
-  );
-}
-
-function Logo() {
-  return <h1>🌴 Far Away 👜</h1>
-}
-
-function Form({onAddItems}) {
-  const [description, setDescription] = useState("");
-  const [quantity, setQuantity] = useState(1);
-
-  function handleSubmit(e) {
-    e.preventDefault();
-
-    if(!description) return;
-    
-    const newItem = {description, quantity, packed: false,
-      id: Date.now() 
-    };
-    console.log(newItem);
-
-    onAddItems(newItem);
-
-    setDescription('');
-    setQuantity(1);
-  }
-
-  return (
-    <form className="add-form" onSubmit={handleSubmit}>
-      <h3>What do you need for your trip? 😁</h3>
-      <select value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>
-        {Array.from({length: 20}, (_, i) => i + 1).
-        map(num=>
-        <option value={num} key={num}>
-          {num}
-        </option>
-      )}
-      </select>
-      <input type='text' placeholder="Item..." value={description}
-      onChange={(e) => setDescription(e.target.value)}
-      />
-      <button>Add</button>
-    </form>
   );
 }
 
