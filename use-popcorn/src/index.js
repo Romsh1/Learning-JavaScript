87f1c7c6bb0417cf1948e7c8124fc079d0429760
Romika <romika2002chau.21@gmail.com>
2024-06-04 23:49:54 -0700

    Added a function star rating - Day 76 of Learning React and JS

--- use-popcorn/src/index.js
+++ use-popcorn/src/index.js
@@ -9,6 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating />
+    <StarRating maxRating={10} />
+    <StarRating maxRating={5} />
   </React.StrictMode>
 );
