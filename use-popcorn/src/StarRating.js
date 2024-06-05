98e7c47b9ed555c2a623fa0146aaa9eadae46c86
Romika <romika2002chau.21@gmail.com>
2024-06-05 01:42:18 -0700

    Function with temporary rating working fine

--- use-popcorn/src/StarRating.js
+++ use-popcorn/src/StarRating.js
@@ -18,6 +18,7 @@ const textStyle = {
 
 function StarRating({ maxRating = 5 }) {
     const [rating, setRating] = useState(0);
+    const [tempRating, setTempRating] = useState(0);
 
     function handleRating(rating) {
         setRating(rating);
@@ -27,10 +28,17 @@ function StarRating({ maxRating = 5 }) {
         <div style={containerStyle}>
             <div style={starContainerStyle}>
                 {Array.from({ length: maxRating }, (_, i) => (
-                    <Star key={i} onRate={() => handleRating(i + 1)} />
+                    <Star 
+                        key={i} 
+                        onRate={() => handleRating(i + 1)}
+                        full={tempRating ? tempRating >= i + 1 :
+                             rating >= i + 1}
+                        onHoverIn={() => setTempRating(i + 1)}
+                        onHoverOut = {() => setTempRating(0)}
+                        />
                 ))}
             </div>
-            <p style={textStyle}>{rating || ''}</p>
+            <p style={textStyle}>{tempRating || ''}</p>
         </div>
     );
 }
@@ -42,9 +50,15 @@ const starStyle = {
     cursor: 'pointer'
 }
 
-function Star({ onRate, full }) {
+function Star({ onRate, full, onHoverIn, onHoverOut }) {
     return (
-        <span role="button" style={starStyle} onClick={onRate}>
+        <span 
+            role="button" 
+            style={starStyle} 
+            onClick={onRate}
+            onMouseEnter={onHoverIn}
+            onMouseLeave={onHoverOut}
+        >
             {full ? <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 20 20"
