7da89aae058634691d97f545d1194db56ec6ed9e
Romika <romika2002chau.21@gmail.com>
2024-06-05 02:31:35 -0700

    Completed rating component

--- use-popcorn/src/StarRating.js
+++ use-popcorn/src/StarRating.js
@@ -17,12 +17,14 @@ function StarRating({ maxRating = 5,
     className = "test",
     messages = [],
     defaultRating = 0,
+    onSetRating
 }) {
     const [rating, setRating] = useState(defaultRating);
     const [tempRating, setTempRating] = useState(0);
 
     function handleRating(rating) {
         setRating(rating);
+        onSetRating(rating);
     }
 
     const textStyle = {
