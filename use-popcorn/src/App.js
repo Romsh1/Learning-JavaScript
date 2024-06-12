6ddb0ac3fad5d4e3dfe20f25bf5ee18a3d2fe3dc
Romika <romika2002chau.21@gmail.com>
2024-06-12 02:28:57 -0700

    Using useEffect hook

--- use-popcorn/src/App.js
+++ use-popcorn/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const tempMovieData = [
   {
@@ -245,10 +245,13 @@ export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
   const [watched, setWatched] = useState(tempWatchedData);
 
-  fetch(`http://www.omdbapi.com/?apikey=${KEY}&
-    s=interstellar`)
-      .then((res) => res.json())
-      .then((data) => console.log(data));
+  useEffect(function() {
+    fetch(`http://www.omdbapi.com/?apikey=${KEY}&
+      s=interstellar`)
+        .then((res) => res.json())
+        .then((data) => console.log(data));  
+  }, []); /* Only run on mount i.e. when App renders for the first time */
+
 
   return (
     <>
