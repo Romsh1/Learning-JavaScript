68e6f293c6b6e07b5c8305a4cc7935e38a02d1db
Romika <romika2002chau.21@gmail.com>
2024-07-01 10:45:02 -0700

    State variable set up

--- currency-converter/src/App.jsx
+++ currency-converter/src/App.jsx
@@ -1,16 +1,23 @@
-import './App.css'
+import { useState } from "react";
 
 function App() {
+  const [amount, setAmount] = useState(1);
+  const [fromCurrency, setFromCurrency] = useState('EUR');
+  const [toCurrency, setToCurrency] = useState('USD');
+
   return (
     <div>
-      <input type='text' />
-      <select>
+      <input type='text'
+       value={amount}
+       onChange={ (e) => setAmount(Number(e.target.value))} 
+      />
+      <select value={fromCurrency} onChange={(e) => setFromCurrency(e.target.value)}>
         <option value="USD">USD</option>
         <option value="CAD">CAD</option>
         <option value="NPR">NPR</option>
         <option value="EUR">EUR</option>
       </select>
-      <select>
+      <select value={toCurrency} onChange={(e) => setToCurrency(e.target.value)}>
         <option value="USD">USD</option>
         <option value="CAD">CAD</option>
         <option value="NPR">NPR</option>
