3469090ea92f4ff0354e0ae6e978352d2402f6db
Romika <romika2002chau.21@gmail.com>
2024-07-04 12:38:10 -0700

    Testing Profile component

--- react-practice/src/App.jsx
+++ react-practice/src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
+import Profile from './Profile.jsx';
 
 function App() {
 
   return (
-    <>
-    </>
+    <Profile />
   )
 }
 
---
+++ react-practice/src/Profile.jsx
@@ -0,0 +1,7 @@
+function Profile() {
+    return (
+        <h2>This is Romika.</h2>
+    );
+}
+
+export default Profile
